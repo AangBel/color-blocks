2b2edb88af7b12387f70319367c3a0e359d01258
Angelica Beltran <angelica.beltran11@gmail.com>
2023-05-04 22:10:37 -0500

    added the block for remove the last four

--- script.js
+++ script.js
@@ -2,70 +2,63 @@ console.log("🟥 🟦 🟩 🟨");
 $(document).ready(onReady);
 
 function onReady() {
-  //console.log("We have jQuery! 💲💲💲");
- $('#red-btn').on('click', addRed);
+  console.log("We have jQuery! 💲💲💲");
+  let redButton = $("#red-btn");
+  let blueButton = $("#blue-btn");
+  let greenButton = $("#green-btn");
+  let yellowButton = $("#yellow-btn");
+  $("#blocks").on("click", ".block", removeBlock);
 
- $('#blocks').on('click','.block', removeRed);
- }//end of onReady
+  $("#red-btn").on("click", redAppend);
+  $("#blue-btn").on("click", blueAppend);
+  $("#green-btn").on("click", greenAppend);
+  $("#yellow-btn").on("click", yellowAppend);
 
- function addRed(){
-    console.log('anything');
-   $('#blocks').append('<div class="block red-fill"></div>');
- } //end appendRed
+  $("#blocks").on("click", ".block", removeBlock);
+} //end of on ready
+
+///----------------------------
+function redAppend() {
+  $("#redContainer").append('<div class="block red-fill"></div>');
+}
+function blueAppend() {
+  $("#blueContainer").append('<div class="block blue-fill"></div>');
+}
+function greenAppend() {
+  $("#greenContainer").append('<div class="block green-fill"></div>');
+}
+function yellowAppend() {
+  $("#yellowContainer").append('<div class="block yellow-fill"></div>');
+}
+///----------------------------
+function removeBlock(){
+  $(this).remove();
+//   redRecount();
+//   blueRecount();
+//   greenRecount();
+//   yellowRecount();
+
+$("#redContainer").on("click", ".block", removeRed);
+$("#blueContainer").on("click", ".block", removeBlue);
+$("#greenContainer").on("click", ".block", removeGreen);
+$("#yellowContainer").on("click", ".block", removeYellow);
+
+}
 
 function removeRed(){
     $(this).remove();
+    // $("#block").on("click", ".block", removeRed);
+}
 
-//  $('#redContainer').on('click', removeRed);
- }//end to removeRed
+function removeBlue(){
+   $(this).remove();
+}
 
- //-----------------------------------------
-//   let redBtn = $("#block red-fill");
-//   console.log("red-Btn:", redBtn);
+function removeGreen(){
+   $(this).remove();
+}
 
-//   let blueBtn = $("#block blue-fill");
-//   console.log("blue-Btn:", blueBtn);
+function removeYellow(){
+  $(this).remove();
+}
 
-//   let greenBtn = $("#block green-fill");
-//   console.log("green-Btn:", greenBtn);
-
-//   let yellowBtn = $("#block yellow-fill");
-//   console.log("yellow-Btn:", yellowBtn);
-
- //end for function onReady
-
-  //end for appendRed
-
-//   function appendBlue() {
-//     $("#blueContainer").append('<div class="block blue-fill"></div>');
-//   } //end for appendBlue
-
-//   function appendGreen() {
-//     $("#greenContainer").append('<div class="block green-fill"></div>');
-//   } //end for appendGreen
-
-//   function appendYellow() {
-//     $("#yellowContainer").append('<div class="block yellow-fill"></div>');
-//   } //end for appendYellow
-
-
-
-// $('#blue-Btn').on('click', appendBlue);
-// $('#blue-fillContainer').on('click').remove(appendBlue);
-// $('#blueContainer').on('click', '.blue' , removeBlue);
-// function removeBlue(){
-//     $(this).remove();
-// }
-
-// $('#green-Btn').on('click', appendGreen);
-// $('#greenContainer').on('click').remove(appendGreen);
-// $('#greenContainer').on('click', '.green' , removeGreen);
-// function removeGreen(){
-//     $(this).remove();
-// }
-
-// $('#yellow-Btn').on('click', appendYellow);
-// $('#yellowContainer').on('click').remove(appendYellow);
-// $('#yellowContainer').on('click', '.yellow' , removeYellow);
-// function removeYellow(){
-//     $(this).remove();
